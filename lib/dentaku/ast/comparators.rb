06f8630143f7840a6098154d4f7f96a1ef2a650e
Jan Mareš <jan.mares@manast.eu>
2016-02-22 14:04:38 +0100

    Add cast() function from Arithmetic for tolerant String values.

--- lib/dentaku/ast/comparators.rb
+++ lib/dentaku/ast/comparators.rb
@@ -10,41 +10,50 @@ module Dentaku
       def type
         :logical
       end
+
+      private
+
+      def cast(value, prefer_integer=true)
+        v = BigDecimal.new(value, Float::DIG+1)
+        v = v.to_i if prefer_integer && v.frac.zero?
+        v
+      end
+
     end
 
     class LessThan < Comparator
       def value(context={})
-        left.value(context) < right.value(context)
+        cast(left.value(context)) < cast(right.value(context))
       end
     end
 
     class LessThanOrEqual < Comparator
       def value(context={})
-        left.value(context) <= right.value(context)
+        cast(left.value(context)) <= cast(right.value(context))
       end
     end
 
     class GreaterThan < Comparator
       def value(context={})
-        left.value(context) > right.value(context)
+        cast(left.value(context)) > cast(right.value(context))
       end
     end
 
     class GreaterThanOrEqual < Comparator
       def value(context={})
-        left.value(context) >= right.value(context)
+        cast(left.value(context)) >= cast(right.value(context))
       end
     end
 
     class NotEqual < Comparator
       def value(context={})
-        left.value(context) != right.value(context)
+        cast(left.value(context)) != cast(right.value(context))
       end
     end
 
     class Equal < Comparator
       def value(context={})
-        left.value(context) == right.value(context)
+        cast(left.value(context)) == cast(right.value(context))
       end
     end
   end
